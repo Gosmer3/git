230c0d1953a728900192532a318af1b414c30fcc
Jacob Gosmer <eaajago@students.dk>
2023-02-17 11:21:29 +0100

    Igen

--- test/src/Test.java
+++ test/src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hej");
         System.out.println("igen");
+        System.out.println("igen igen");
     }
 }
